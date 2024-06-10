d37ed065037818f23ce2ebe0745e5323aeadd78e
BHASKAR SAMANTA <129597366+BhaskarSamanta@users.noreply.github.com>
2024-06-10 17:49:39 +0530

    tested the app

--- InventoryManagementSystem/src/App.jsx
+++ InventoryManagementSystem/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1 className='text-3xl font-semibold underline'>
+        This is Inventory Management System
+      </h1>
     </>
   )
 }
