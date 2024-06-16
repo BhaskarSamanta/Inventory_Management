18ea985a786df791347cf80dc520a0f5596e379b
BHASKAR SAMANTA <129597366+BhaskarSamanta@users.noreply.github.com>
2024-06-16 18:19:17 +0530

    compleated the database page

--- InventoryManagementSystem/src/appwrite/config.js
+++ InventoryManagementSystem/src/appwrite/config.js
@@ -14,63 +14,65 @@ export class AppwriteService{
         this.bucket = new Storage(this.client);
     }
 
-     async addProduct ({Product_name, slug, User_id, Product_price, Product_quantity, Product_description}) {
+     async addProduct ({Product_Name, Product_ID, User_ID, Price, Description, Stock_Qty, Supplier_ID,Category_ID}) {
         try {
             return await this.databases.createDocument(
                 conf.appwrite_DatabaseId, 
-                conf. appwrite_Pruduct_CollectionId, 
-                slug,
+                conf.appwrite_Pruduct_CollectionId, 
+                Product_ID,
                 {
-                    Product_name, 
-                    Product_price, 
-                    Product_quantity, 
-                    Product_description,
-                    User_id
+                    Product_Name, 
+                    Price, 
+                    Description,
+                    User_ID,
+                    Stock_Qty,
+                    Supplier_ID,
+                    Category_ID
                 });
         } catch (error) {
-            return ("Appwrite serive :: addProduct :: error", error)
+            throw ("Appwrite serive :: addProduct :: error", error)
         }
     }
 
-    async updateProduct(slug,{ Product_name, Product_price, Product_quantity, Product_description}){
+    async updateProduct(Product_ID,{ Product_Name, Price, Stock_Qty, Description}){
         try {
             return await this.databases.updateDocument(
                 conf.appwrite_DatabaseId,
                 conf.appwrite_Pruduct_CollectionId,
-                slug,
+                Product_ID,
                 {
-                    Product_name, 
-                    Product_price, 
-                    Product_quantity, 
-                    Product_description,
+                    Product_Name, 
+                    Price, 
+                    Stock_Qty, 
+                    Description,
                 }
             )
         } catch (error) {
-            return ("Appwrite serive :: deleteProduct :: error",error);
+            throw ("Appwrite serive :: deleteProduct :: error",error);
         }
     }
 
-    async deleteProduct(slug){
+    async deleteProduct(Product_ID){
         try {
             return await this.databases.deleteDocument(
                 conf.appwrite_DatabaseId,
                 conf.appwrite_Pruduct_CollectionId,
-                slug
+                Product_ID
             )
         } catch (error) {
-            return ("Appwrite serive :: deletePost :: error",error);         
+            throw ("Appwrite serive :: deletePost :: error",error);         
         }
     }
 
-    async getProduct(slug){
+    async getProduct(Product_ID){
         try {
             return await this.databases.getDocument(
                 conf.appwrite_DatabaseId,
                 conf.appwrite_Pruduct_CollectionId,
-                slug
+                Product_ID
             );
         } catch (error) {
-            return ("Appwrite serive :: getProduct :: error",error);
+            throw ("Appwrite serive :: getProduct :: error",error);
         }
         
     }
@@ -83,7 +85,202 @@ export class AppwriteService{
                 querys
             )
         } catch (error) {
-            return ("Appwrite serive :: getProducts :: error",error);
+            throw ("Appwrite serive :: getProducts :: error",error);
         }
     }
+    async addCatagory({Category_Name, Category_ID}){
+        try {
+            return await this.databases.createDocument(
+               conf.appwrite_DatabaseId,
+               conf.appwrite_Category_CollectionId,
+               Category_ID,
+               Category_Name
+
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: addCatagory :: error",error);
+        }
+    }
+
+    async deleteCatagory(Category_ID){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Category_CollectionId,
+                Category_ID
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: deleteCatagory :: error",error);
+        }
+    }
+
+    async getCatagory(Category_ID){
+        try {
+            return await this.databases.getDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Category_CollectionId,
+                Category_ID
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: getCatagory :: error",error);
+        }
+    }
+
+    async addSupplier(Supplier_ID,{Supplier_Name, Address, Contact}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Supplier_CollectionId,
+                Supplier_ID,
+                {
+                    Supplier_Name,
+                    Address,
+                    Contact
+                }
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: addSupplier :: error",error);
+        }
+    }
+
+    async deleteSupplier(Supplier_ID){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Supplier_CollectionId,
+                Supplier_ID
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: deleteSupplier :: error",error);
+        }
+    }
+
+    async updateSupplier(Supplier_ID,{Supplier_Name, Address, Contact}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Supplier_CollectionId,
+                Supplier_ID,
+                {
+                    Supplier_Name,
+                    Address,
+                    Contact
+                }
+
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: updateSupplier :: error",error);
+        }
+    }
+
+    async getSupplier(Supplier_ID){
+        try {
+            return await this.databases.getDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Pruduct_CollectionId,
+                Supplier_ID
+            );
+        } catch (error) {
+            throw ("Appwrite serive :: getProduct :: error",error);
+        }
+        
+    }
+
+    async getSuppliers(querys = Query.equal("Supplier_ID", [true])){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Supplier_CollectionId,
+                querys
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: getSuppliers :: error",error);
+        }
+    }
+
+    async addPurchaseOrder(Order_Id,{Total_Amount,Order_Date,supplier_Id}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Purchase_Order_CollectionId,
+                Order_Id,
+                {
+                    Total_Amount,
+                    Order_Date,
+                    supplier_Id
+                }
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: addPerchaseOrder :: error",error);
+        }
+
+    }
+
+    async deletePurchaseOrder(Order_Id){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Purchase_Order_CollectionId,
+                Order_Id
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: deletePerchaseOrder :: error",error);
+        }
+    }
+
+    async getPurchaseOrder(Order_Id){
+        try {
+            return await this.databases.getDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Purchase_Order_CollectionId,
+                Order_Id
+            )
+        } catch (error) {
+            throw ("Appwrite serive :: getPerchaseOrder :: error",error);
+        }
+    }
+
+    async addPurchaseOrderdetails(Detail_Id,{Quantity,Unit_Price,Total_Price, purchaseOrder, Product_ID}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Purchase_Order_Detail_CollectionId,
+                Detail_Id,
+                {
+                    Quantity,
+                    Unit_Price,
+                    Total_Price,
+                    purchaseOrder,
+                    Product_ID
+                }
+            )
+        } catch (error) {
+            throw("Appwrite serive :: addPerchaseOrderdetails :: error",error);
+        }
+    }
+
+    async deletePurchaseOrderdetails(Detail_Id){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Purchase_Order_Detail_CollectionId,
+                Detail_Id
+            )
+        } catch (error) {
+            throw("Appwrite serive :: deletePerchaseOrderdetails :: error",error);
+        }
+    }
+
+    async getPurchaseOrderdetails(Detail_Id){
+        try {
+            return await this.databases.getDocument(
+                conf.appwrite_DatabaseId,
+                conf.appwrite_Purchase_Order_Detail_CollectionId,
+                Detail_Id
+            )
+        } catch (error) {
+            throw("Appwrite serive :: getPerchaseOrderdetails :: error",error);
+        }
+    }
+   
 }
