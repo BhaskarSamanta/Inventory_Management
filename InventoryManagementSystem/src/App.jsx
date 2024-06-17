d2feeb161ff50c3a83bd7e6f9df18b9e0d3299e1
BHASKAR SAMANTA <129597366+BhaskarSamanta@users.noreply.github.com>
2024-06-17 16:19:09 +0530

    installed the shadcn

--- InventoryManagementSystem/src/App.jsx
+++ InventoryManagementSystem/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl font-semibold underline'>
-        This is Inventory Management System
-      </h1>
-    </>
+    <div>
+      <Button
+          type="submit"
+          variant="default" // Change to any variant you need
+          size="default"   // Change to any size you need
+          className="mt-4"
+        >
+          Log In
+        </Button>
+    </div>
   )
 }
 
